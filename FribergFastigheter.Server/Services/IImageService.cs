cb91089e0502e0fdcae18e044f8ddf120b9ad34d
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-18 10:54:13 +0200

    Build a service for handeling images with corresponding interface.

---
+++ FribergFastigheter.Server/Services/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using FribergFastigheter.Server.Data.DTO;
+using FribergFastigheterApi.Data.Entities;
+
+namespace FribergFastigheter.Server.Services
+{
+    public interface IImageService
+    {
+        void DeleteImageFromDisk(string fileName);
+        Image SaveImageToDisk(ImageDto imageDto);
+        void SetImageData(ImageDto image);
+        void SetImageData(List<ImageDto> imageList);
+    }
+}
