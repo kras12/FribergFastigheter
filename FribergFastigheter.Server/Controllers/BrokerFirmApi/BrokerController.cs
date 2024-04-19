0c0c560927cd8ab6993e0327e8e2ea7cf05b038e
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-19 09:56:13 +0200

    Fixed errormessages.

--- FribergFastigheter.Server/Controllers/BrokerFirmApi/BrokerController.cs
+++ FribergFastigheter.Server/Controllers/BrokerFirmApi/BrokerController.cs
@@ -6,6 +6,7 @@ using FribergFastigheter.Server.Data.Repositories;
 using FribergFastigheter.Server.Services;
 using FribergFastigheterApi.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 
@@ -97,7 +98,7 @@ namespace FribergFastigheter.Server.Controllers.BrokerFirmApi
 
             if (broker == null)
             {
-                return NotFound();
+                return NotFound(new ErrorMessageDto(System.Net.HttpStatusCode.NotFound, $"The broker with ID '{brokerId}' was not found."));
             }
 
             var result = _mapper.Map<BrokerDto>(broker);
@@ -142,7 +143,7 @@ namespace FribergFastigheter.Server.Controllers.BrokerFirmApi
         {
             if (brokerId != updateBrokerDto.BrokerId)
             {
-                return BadRequest();
+                return BadRequest(new ErrorMessageDto(HttpStatusCode.BadRequest, "You have not permission to change referenced broker object."));
             }
 
             var broker = _mapper.Map<Broker>(updateBrokerDto);
@@ -173,7 +174,12 @@ namespace FribergFastigheter.Server.Controllers.BrokerFirmApi
             await _brokerRepository.DeleteAsync(id);
             return Ok();
         }
+
+        #endregion
+
+    
     }
+
 }
 
-#endregion
+
