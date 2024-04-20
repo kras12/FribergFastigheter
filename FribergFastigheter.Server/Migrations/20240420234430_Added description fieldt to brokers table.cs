b169b8edd63349459ef251d8ac81014e22b11783
runehero <runehero@hotmail.com>
2024-04-21 01:45:02 +0200

    Added description field to brokers table.

---
+++ FribergFastigheter.Server/Migrations/20240420234430_Added description fieldt to brokers table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FribergFastigheterApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addeddescriptionfieldttobrokerstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Brokers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Brokers");
+        }
+    }
+}
