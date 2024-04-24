5e53541df4d8288b418d2978ab17ce5fcba382d9
runehero <runehero@hotmail.com>
2024-04-24 22:02:46 +0200

    Added a new component to serve the home page and continued to work on implementing the page.

---
+++ FribergFastigheter.Client/Components/HousingSearchResultList.razor.cs
@@ -0,0 +1,22 @@
+﻿using FribergFastigheter.Client.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FribergFastigheter.Client.Components
+{
+    /// <summary>
+    /// Component that shows a housing search listing
+    /// </summary>
+    public partial class HousingSearchResultList : ComponentBase
+    {
+        #region Properties
+
+        [Parameter]
+        public List<HousingViewModel> Housings { get; set; } = new();
+
+        #endregion
+
+        #region Methods        
+
+        #endregion
+    }
+}
---
+++ FribergFastigheter.Client/HelperClasses/StringFormatHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace FribergFastigheter.Client.HelperClasses
+{
+    /// <summary>
+    /// Helper class to format strings.
+    /// </summary>
+    public static class StringFormatHelper
+    {
+        #region Constants
+
+        /// <summary>
+        /// The default format string for displaying price with 2 decimals, thousands separator, and currency.
+        /// </summary>
+        private const string PriceOutputFormatString = "{0:N2} kr";
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Formats the number as a price string.
+        /// </summary>
+        /// <param name="price">The price to format.</param>
+        /// <param name="includeDecimals">True to format the price with 2 decimals.</param>
+        /// <returns>A formatted <see cref="string"/>.</returns>
+        /// <!-- Author: Jimmie -->
+        /// <!-- Co Authors: -->
+        public static string FormatPrice(decimal price, bool includeDecimals = false)
+        {
+            if (includeDecimals)
+            {
+                // price with 2 decimals, thousands separator, and currency.
+                return $"{price:C2}";
+            }
+            else
+            {
+                // price with 0 decimals, thousands separator, and currency.
+                return $"{price:C0}";
+            }            
+        }
+
+        #endregion
+
+    }
+}
