5da2260ad02cb88acbf80bc14163c3aa7b24e8f6
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-24 15:48:07 +0200

    Created autoMapperProfiles on Client. Added mapping for BrokerDTO to BrokerViewModel both ways. Added mapping for ImageDTO to ImageViewModel both ways.

---
+++ FribergFastigheter.Client/AutoMapper/DtoToViewModelAutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Shared.Dto;
+
+namespace FribergFastigheter.Client.AutoMapper
+{
+    public class DtoToViewModelAutoMapperProfile : Profile
+    {
+        public DtoToViewModelAutoMapperProfile()
+        {
+            CreateMap<BrokerDto, BrokerViewModel>()
+                .ForMember(dest => dest.ProfileImage, opt => opt
+                .PreCondition(x => x.ProfileImage != null));
+
+            CreateMap<ImageDto, ImageViewModel>();
+        }
+    }
+}
---
+++ FribergFastigheter.Client/AutoMapper/ViewModelToDtoAutoMapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Shared.Dto;
+
+namespace FribergFastigheter.Client.AutoMapper
+{
+    /// <summary>
+	/// An auto mapper profile that contains mappings for converting entity classes to DTO classes.
+	/// </summary>
+	/// <!-- Author: Marcus -->
+	/// <!-- Co Authors: -->
+    public class ViewModelToDtoAutoMapperProfile : Profile
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// 
+        public ViewModelToDtoAutoMapperProfile()
+        {
+            CreateMap<BrokerViewModel, BrokerDto>()
+                .ForMember(dest => dest.ProfileImage, opt => opt.PreCondition(x => x.ProfileImage != null));
+        }
+    }
+}
