e2b19d5ef67361285e6f903dcf1ce5f5e00f9811
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-24 15:49:24 +0200

    Added Models för Client. BrokerViewModel ImageViewModel

---
+++ FribergFastigheter.Client/Models/BrokerViewModel.cs
@@ -0,0 +1,63 @@
+﻿using FribergFastigheter.Shared.Dto;
+using System.ComponentModel.DataAnnotations;
+
+namespace FribergFastigheter.Client.Models
+{
+    public class BrokerViewModel
+    {
+        #region Constructors
+
+        /// <summary>
+        /// A constructor.
+        /// </summary>
+        public BrokerViewModel()
+        {
+            
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The firm that the broker belongs to.
+        /// </summary>
+        public BrokerFirmDto BrokerFirm { get; set; }
+
+        /// <summary>
+        /// The ID of the broker.
+        /// </summary>
+        public int BrokerId { get; set; }
+
+        /// <summary>
+        /// The email of the broker.
+        /// </summary>
+        public string Email { get; set; } = "";
+
+        /// <summary>
+        /// The description of the broker.
+        /// </summary>
+        public string Description { get; set; } = "";
+
+        /// <summary>
+        /// The first name of the broker.
+        /// </summary>
+        public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// The last name of the broker.
+        /// </summary>
+        public string LastName { get; set; } = "";
+        /// <summary>
+        /// The phone number of the broker.
+        /// </summary>
+        public string PhoneNumber { get; set; } = "";
+
+        /// <summary>
+        /// The broker profile image.
+        /// </summary>
+        public ImageViewModel? ProfileImage { get; set; } = null;
+
+        #endregion
+    }
+}
---
+++ FribergFastigheter.Client/Models/ImageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FribergFastigheter.Shared.Enums;
+
+namespace FribergFastigheter.Client.Models
+{
+    public class ImageViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// The filename of the image.
+        /// </summary>
+        public string FileName { get; set; } = "";
+
+        /// <summary>
+        /// The image type.
+        /// </summary>
+        public ImageTypes ImageType { get; set; }
+
+        /// <summary>
+        /// The ID of the image.
+        /// </summary>
+        public int ImageId { get; set; }
+
+        /// <summary>
+        /// The url of the image.
+        /// </summary>
+        public string Url { get; set; } = "";
+
+        #endregion
+    }
+}
