0a2cffb7413cc085513975b196d4ae721cc0b972
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-26 13:31:43 +0200

    Added a page for the BrokerDetails.

--- FribergFastigheter.Client/Pages/BrokerFirmMember/Broker/BrokerDetailsPage.razor.cs
+++ FribergFastigheter.Client/Pages/BrokerFirmMember/Broker/BrokerDetailsPage.razor.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Client.Services.HousingApi;
+using FribergFastigheter.Shared.Dto;
+using Microsoft.AspNetCore.Components;
 
 namespace FribergFastigheter.Client.Pages.BrokerFirmMember.Broker
 {
@@ -8,10 +12,36 @@ namespace FribergFastigheter.Client.Pages.BrokerFirmMember.Broker
     public partial class BrokerDetailsPage : ComponentBase
     {
         #region Properties
-        
         [Parameter]
         public int Id { get; set; }
+        public int BrokerFirmId { get; set; } = 1;
+        BrokerViewModel Broker { get; set; }
+        [Inject]
+        public IBrokerApiService BrokerApiService { get; set; }
+        [Inject]
+        public IMapper Mapper { get; set; }
 
-		#endregion
-	}
+        #endregion
+        #region Constructors
+
+        public BrokerDetailsPage()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override async Task OnInitializedAsync()
+        {
+            BrokerDto? broker = (await BrokerApiService.GetBrokerById(Id, BrokerFirmId));
+            BrokerViewModel result = Mapper.Map<BrokerViewModel>(broker);
+            Broker = result;
+        }
+
+        #endregion
+
+
+    }
 }
