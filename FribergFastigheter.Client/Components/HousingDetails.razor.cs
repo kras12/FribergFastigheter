13025b0726a68b2ea0653a7a03d71ec318200b3d
runehero <runehero@hotmail.com>
2024-04-26 14:33:56 +0200

    Implemented housing details page.

---
+++ FribergFastigheter.Client/Components/HousingDetails.razor.cs
@@ -0,0 +1,30 @@
+﻿using FribergFastigheter.Client.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FribergFastigheter.Client.Components
+{
+    /// <summary>
+    /// Component that shows housing details.
+    /// </summary>
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public partial class HousingDetails
+    {
+        #region Properties
+
+        [Parameter]
+#pragma warning disable CS8618 
+        public HousingViewModel Housing { get; set; } = new();
+#pragma warning restore CS8618
+
+        public List<ImageSlideViewModel> ImageSlides
+        {
+            get
+            {
+                return Housing.Images.Select(x => new ImageSlideViewModel(x)).ToList();
+            }
+        }
+
+        #endregion
+    }
+}
