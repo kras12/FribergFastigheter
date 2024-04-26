f77784feaa492b5abb3424e7d9152ab2bffb739b
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-26 13:27:40 +0200

    Built a component for showing Broker details.

---
+++ FribergFastigheter.Client/Components/BrokerDetails.razor.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Client.Services.HousingApi;
+using FribergFastigheter.Shared.Dto;
+using Microsoft.AspNetCore.Components;
+
+namespace FribergFastigheter.Client.Components
+{
+    public partial class BrokerDetails : ComponentBase
+    {
+        #region Properties
+
+        [Parameter]
+        public BrokerViewModel Broker { get; set; }
+        
+        #endregion
+        #region Constructors
+
+        public BrokerDetails()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
+
