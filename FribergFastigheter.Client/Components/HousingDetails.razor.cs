08e503f9f2d9d3893d034174a5913e7af5be7ea7
runehero <runehero@hotmail.com>
2024-04-27 22:03:18 +0200

    Fixed switching between housing details and the image gallery within the same component. Also tweaked the styling of the component output.

--- FribergFastigheter.Client/Components/HousingDetails.razor.cs
+++ FribergFastigheter.Client/Components/HousingDetails.razor.cs
@@ -1,5 +1,6 @@
 ﻿using FribergFastigheter.Client.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace FribergFastigheter.Client.Components
 {
@@ -12,17 +13,51 @@ namespace FribergFastigheter.Client.Components
     {
         #region Properties
 
+        /// <summary>
+        /// The housing object to show details for. 
+        /// </summary>
         [Parameter]
 #pragma warning disable CS8618 
         public HousingViewModel Housing { get; set; } = new();
+
 #pragma warning restore CS8618
 
+        /// <summary>
+        /// Returns image slides to show in the image slide show. 
+        /// </summary>
         public List<ImageSlideViewModel> ImageSlides
         {
             get
             {
                 return Housing.Images.Select(x => new ImageSlideViewModel(x)).ToList();
             }
+        }
+
+        /// <summary>
+        /// Returns true if the image gallery has been opened. 
+        /// </summary>
+        public bool IsImageGalleryActive { get; set; } = false;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Closes the image gallery and returns to the housing details.
+        /// </summary>
+        /// <!-- Author: Jimmie -->
+        /// <!-- Co Authors: -->
+        public void CloseImageGallery()
+        {
+            IsImageGalleryActive = false;
+        }
+
+        /// <summary>
+        /// Opens the image gallery.
+        /// </summary>
+        public void OpenImageGallery()
+        {
+            IsImageGalleryActive = true;
         }
 
         #endregion
