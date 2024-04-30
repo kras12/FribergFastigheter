01317ee6bb58244dba569f0811effe2290fe8c85
runehero <runehero@hotmail.com>
2024-04-30 13:27:29 +0200

    New auto mapper profile to map between view model classes.

---
+++ FribergFastigheter.Client/AutoMapper/ViewModelToViewModelAutoMapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Shared.Dto;
+
+namespace FribergFastigheter.Client.AutoMapper
+{
+	/// <summary>
+	/// An auto mapper profile that contains mappings for converting view model classes to other view model classes classes.
+	/// </summary>
+	/// <!-- Author: Jimmie -->
+	/// <!-- Co Authors: -->
+	public class ViewModelToViewModelAutoMapperProfile : Profile
+	{
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ViewModelToViewModelAutoMapperProfile()
+		{
+            CreateMap<HousingViewModel, EditHousingViewModel>()
+                .ForMember(x => x.BrokerFirmId, opt => opt.MapFrom(src => src.Broker.BrokerFirm.BrokerFirmId))
+                .ForMember(x => x.BrokerId, opt => opt.MapFrom(src => src.Broker.BrokerId))
+                .ForMember(x => x.SelectedMunicipalityId, opt => opt.MapFrom(src => src.Municipality.MunicipalityId))
+                .ForMember(x => x.SelectedCategoryId, opt => opt.MapFrom(src => src.Category.HousingCategoryId));
+        }		
+	}
+}
