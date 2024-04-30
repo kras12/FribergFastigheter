e285b8f88bb3822a83581ad020c1fdc58daa4d66
runehero <runehero@hotmail.com>
2024-04-30 13:34:47 +0200

    Added a call to notify the component of state changed to the scroll to element method.

--- FribergFastigheter.Client/Components/HousingList.razor.cs
+++ FribergFastigheter.Client/Components/HousingList.razor.cs
@@ -25,21 +25,41 @@ namespace FribergFastigheter.Client.Components
         [Parameter]
         public List<HousingViewModel> Housings { get; set; } = new();
 
+#pragma warning disable CS8618
         /// <summary>
         /// Injected JavaScript runtime.
         /// </summary>
         [Inject]
         public IJSRuntime JSRuntime {  get; set; }
+#pragma warning restore CS8618 
 
         #endregion
 
         #region Method
 
-        public async Task ScrollToElement(HousingViewModel createdHousing)
+        /// <summary>
+        /// Scrolls to the element.
+        /// </summary>
+        /// <param name="createdHousing"></param>
+        public void ScrollToElement(HousingViewModel createdHousing)
         {
             _scrollToELementId = $"HousingListItem-{createdHousing.HousingId}";
+            StateHasChanged();
         }
 
+        /// <summary>
+        /// Method invoked after each time the component has been rendered interactively
+        /// and the UI has finished updating (for example, after elements have been added
+        /// to the browser DOM). Any Microsoft.AspNetCore.Components.ElementReference fields
+        /// will be populated by the time this runs. This method is not invoked during prerendering
+        /// or server-side rendering, because those processes are not attached to any live
+        /// browser DOM and are already complete before the DOM is updated. Note that the
+        /// component does not automatically re-render after the completion of any returned
+        /// System.Threading.Tasks.Task, because that would cause an infinite render loop.
+        /// </summary>
+        /// <param name="firstRender">Set to true if this is the first time Microsoft.AspNetCore.Components.ComponentBase.OnAfterRender(System.Boolean)
+        ///     has been invoked on this component instance; otherwise false.</param>
+        /// <returns>A System.Threading.Tasks.Task representing any asynchronous operation.</returns>
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             await base.OnAfterRenderAsync(firstRender);
