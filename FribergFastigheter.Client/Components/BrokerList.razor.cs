df002371b66699a54aa8a7328090b01a81e8a7b9
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-04-30 10:39:31 +0200

    Made a Component from the code behind in BrokerListPage. So the List can be used in more places.

---
+++ FribergFastigheter.Client/Components/BrokerList.razor.cs
@@ -0,0 +1,30 @@
+﻿using FribergFastigheter.Client.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FribergFastigheter.Client.Components
+{
+    public partial class BrokerList : ComponentBase
+    {
+        #region Properties
+
+        [Parameter]
+        public List<BrokerViewModel> Brokers { get; set; }
+        [Parameter]
+        public BrokerFirmSummaryViewModel BrokerFirm { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BrokerList()
+        {
+                
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
