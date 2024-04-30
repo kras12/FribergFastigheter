4c16ed16224ddabf2eb195bdfbaefe36df4d27c1
runehero <runehero@hotmail.com>
2024-04-30 11:29:19 +0200

    Created basic functionality for broker back office pages for listing and creating housings.

---
+++ FribergFastigheter.Client/Pages/BrokerFirmMember/Housing/CreateHousingPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Client.Services.FribergFastigheterApi;
+using FribergFastigheter.Shared.Dto;
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace FribergFastigheter.Client.Pages.BrokerFirmMember.Housing
+{
+    /// <summary>
+    /// The create broker housing back office page.
+    /// </summary>
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public partial class CreateHousingPage : ComponentBase
+	{
+
+	}
+}
