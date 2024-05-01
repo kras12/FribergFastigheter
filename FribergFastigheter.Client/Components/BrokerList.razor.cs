b2d4b01efc833c5edf141762a47ea1c7d55bfaad
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-05-01 12:55:28 +0200

    Made a Component for creating a broker. Made changes to BrokerList where the new Component is implemented.

--- FribergFastigheter.Client/Components/BrokerList.razor.cs
+++ FribergFastigheter.Client/Components/BrokerList.razor.cs
@@ -1,16 +1,52 @@
-﻿using FribergFastigheter.Client.Models;
+﻿using AutoMapper;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Client.Services.FribergFastigheterApi;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace FribergFastigheter.Client.Components
 {
+    /// <summary>
+    /// A page for listing brokers.
+    /// </summary>
+    /// <!-- Author: Marcus -->
+    /// <!-- Co Authors: -->
+    /// 
     public partial class BrokerList : ComponentBase
     {
+        #region InjectedServiceProperties
+
+        /// <summary>
+        /// The injected Auto Mapper service. 
+        /// </summary>
+        [Inject]
+        private IMapper Mapper { get; set; }
+        /// <summary>
+        /// The injected housing API service.
+        /// </summary>
+        [Inject]
+        private IBrokerFirmApiService BrokerFirmApiService { get; set; }
+
+        /// <summary>
+        /// Injected JavaScript runtime.
+        /// </summary>
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        #endregion
         #region Properties
 
         [Parameter]
         public List<BrokerViewModel> Brokers { get; set; }
         [Parameter]
         public BrokerFirmSummaryViewModel BrokerFirm { get; set; }
+
+        public bool? IsNewBrokerFormActive { get; set; } = null;
+
+      
+        private BrokerList brokerListComponent;
+
+        private string? _scrollToELementId = null;
 
         #endregion
 
@@ -24,6 +60,44 @@ namespace FribergFastigheter.Client.Components
         #endregion
 
         #region Methods
+
+
+        public void OpenCreateNewBroker()
+        {
+            IsNewBrokerFormActive = true;
+        }
+
+        public void CloseCreateNewBroker(bool close)
+        {
+            if(close == true)
+            {
+                IsNewBrokerFormActive = false;
+            }
+            
+        }
+
+        public async void OnCreatedBroker(BrokerViewModel newBroker) 
+        {
+            Brokers.Add(newBroker);
+            IsNewBrokerFormActive = false;
+            await ScrollToElement(newBroker);
+        }
+
+        public async Task ScrollToElement(BrokerViewModel newBroker)
+        {
+            _scrollToELementId = $"BrokerListItem-{newBroker.BrokerId}";
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (_scrollToELementId != null)
+            {
+                await JSRuntime.InvokeVoidAsync("scrollToElement", _scrollToELementId);
+                _scrollToELementId = null;
+            }
+        }
 
         #endregion
     }
