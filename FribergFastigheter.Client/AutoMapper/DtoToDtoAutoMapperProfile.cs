d3848d4166cb2a6f07d492b2296b3e2f3155fb4a
runehero <runehero@hotmail.com>
2024-05-01 22:18:19 +0200

    New auto mapper profile class for DTO to DTO conversion at the client side.

---
+++ FribergFastigheter.Client/AutoMapper/DtoToDtoAutoMapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FribergFastigheter.Client.Components;
+using FribergFastigheter.Client.Models;
+using FribergFastigheter.Shared.Dto;
+
+namespace FribergFastigheter.Client.AutoMapper
+{
+	/// <summary>
+	/// An auto mapper profile that contains mappings for converting DTO classes to other DTO classes.
+	/// </summary>
+	/// <!-- Author: Jimmie -->
+	/// <!-- Co Authors: -->
+	public class DtoToDtoAutoMapperProfile : Profile
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public DtoToDtoAutoMapperProfile()
+		{
+			CreateMap<HousingDto, DeleteImagesDto>()
+				.ForMember(dest => dest.ImageIds, opt => opt.MapFrom(src => src.Images.Select(x => x.ImageId).ToList()));
+        }
+	}
+}
