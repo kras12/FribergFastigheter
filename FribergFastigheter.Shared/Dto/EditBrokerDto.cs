4283586e2a4d690593325374f7b0b55534ab5c48
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-05-02 14:27:04 +0200

    Made functions for editing brokers and the function for adding a profile image when a new broker is created. This include changes to the EditBrokerDto on the shared project.

---
+++ FribergFastigheter.Shared/Dto/EditBrokerDto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FribergFastigheter.Shared.Dto
+{
+    public class EditBrokerDto
+    {
+        #region Constructors
+
+        /// <summary>
+        /// A constructor.
+        /// </summary>
+        public EditBrokerDto()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The ID of the broker.
+        /// </summary>
+        public int BrokerId { get; set; }
+
+        /// <summary>
+        /// The firm that the broker belongs to.
+        /// </summary>
+        public int BrokerFirmId { get; set; }
+
+        /// <summary>
+        /// The email of the broker.
+        /// </summary>
+        public string Email { get; set; } = "";
+
+        /// <summary>
+        /// The description of the broker.
+        /// </summary>
+        public string Description { get; set; } = "";
+
+        /// <summary>
+        /// The first name of the broker.
+        /// </summary>
+        public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// The last name of the broker.
+        /// </summary>
+        public string LastName { get; set; } = "";
+        /// <summary>
+        /// The phone number of the broker.
+        /// </summary>
+        public string PhoneNumber { get; set; } = "";
+
+        #endregion
+    }
+}
+
