1896618925d8b0044e73beeff14d85c456c34993
runehero <runehero@hotmail.com>
2024-05-03 18:19:13 +0200

    Implented the home page of the broker firm back office.

--- FribergFastigheter.Client/Pages/BrokerFirmMember/BrokerFirmHomePage.razor.cs
+++ FribergFastigheter.Client/Pages/BrokerFirmMember/BrokerFirmHomePage.razor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AutoMapper;
+using FribergFastigheter.Client.Services.FribergFastigheterApi;
+using FribergFastigheter.Shared.Dto.Statistics;
+using Microsoft.AspNetCore.Components;
 using System.ComponentModel;
 
 namespace FribergFastigheter.Client.Pages.BrokerFirmMember
@@ -6,13 +9,63 @@ namespace FribergFastigheter.Client.Pages.BrokerFirmMember
     /// <summary>
     /// The home page of the broker back office. 
     /// </summary>
-    public partial class BrokerHomePage : Component
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public partial class BrokerFirmHomePage : ComponentBase
     {
+        #region Fields
+
+        /// <summary>
+        /// Temporary variable to hold the broker firm ID until identity is implemented. 
+        /// TODO - Delete when identity is implemented. 
+        /// </summary>
+        private int _brokerFirmId = 1;
+
+        /// <summary>
+        /// The statistics for the broker firm. 
+        /// </summary>
+        private BrokerFirmStatisticsViewModel? _brokerFirmStatistics = null;
+
+        #endregion
+
         #region Properties
-        
+
+        /// <summary>
+        /// The injected auto mapper service.
+        /// </summary>
+        [Inject]
+#pragma warning disable CS8618 
+        private IMapper AutoMapper { get; set; }
+#pragma warning restore CS8618 
+
+        /// <summary>
+        /// The injected broker firm API service. 
+        /// </summary>
+        [Inject]
+#pragma warning disable CS8618 
+        private IBrokerFirmApiService BrokerFirmApiService { get; set; }
+#pragma warning restore CS8618 
+
         [Parameter]
         public int Id { get; set; }
 
-		#endregion
-	}
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Method invoked when the component is ready to start, having received its
+        /// initial parameters from its parent in the render tree.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
+        /// <!-- Author: Jimmie -->
+        /// <!-- Co Authors: -->
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            _brokerFirmStatistics = AutoMapper.Map<BrokerFirmStatisticsViewModel>(await BrokerFirmApiService.GetBrokerFirmStatistics(_brokerFirmId));
+        }
+
+        #endregion
+    }
 }
