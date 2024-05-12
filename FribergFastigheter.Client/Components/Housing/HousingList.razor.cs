335bbacb83e70be29775c7a8a11aa591c55b2eba
runehero <runehero@hotmail.com>
2024-05-12 13:55:41 +0200

    The HousingList component is now using the new CanCreateHousing policy, has a new parameter to turn on/off the editing controls, and also got support for filtering the housing list by invidiual brokers.

--- FribergFastigheter.Client/Components/Housing/HousingList.razor.cs
+++ FribergFastigheter.Client/Components/Housing/HousingList.razor.cs
@@ -1,43 +1,70 @@
-﻿using FribergFastigheter.Client.Models.Housing;
+﻿using FribergFastigheter.Client.Models.Broker;
+using FribergFastigheter.Client.Models.Housing;
+using FribergFastigheter.Client.Services.FribergFastigheterApi;
+using FribergFastigheter.Shared.Constants;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 
 namespace FribergFastigheter.Client.Components.Housing
 {
     /// <summary>
-    /// A component that shows a housing list.
+    /// A component that shows a housing list with support for filtering by broker.
     /// </summary>
     /// <!-- Author: Jimmie -->
     /// <!-- Co Authors: -->
     public partial class HousingList : ComponentBase
     {
-        #region Fields
+        #region Enums
 
         /// <summary>
-        /// Temporary ID variable for which broker firm to use performing actions.
-        /// TODO - Replace with data from identity later.
+        /// The types of filtering that can be performed. 
         /// </summary>
-        private int _brokerFirmId = 1;
+        public enum FilterTypes
+        {
+            All,
+            Brokers
+        }
 
-        /// <summary>
-        /// The ID of the broker to assign when creating a new housing object.
-        /// TODO - Replace with data from identity later.
-        /// </summary>
-        private int _createHousingBrokerId = 1;
+		#endregion
 
-        /// <summary>
-        /// Returns true if the create housing component is shown.
-        /// </summary>
-        private bool _isCreatingHousing = false;
+		#region Fields
+
+		/// <summary>
+		/// The ID of the broker to assign when creating a new housing object.
+		/// TODO - Replace with data from identity later.
+		/// </summary>
+		private int _createHousingBrokerId = 1;
+
+		/// <summary>
+		/// The current filter applied to the list.
+		/// </summary>
+		private FilterTypes _currentListFilter = FilterTypes.All;
+
+		/// <summary>
+		/// The filtered housing list that will be displayed.
+		/// </summary>
+		private List<HousingViewModel> _filteredhousings = new();
+
+		/// <summary>
+		/// Returns true if the create housing component is shown.
+		/// </summary>
+		private bool _isCreatingHousing = false;
 
         /// <summary>
         /// An element ID to scroll to on next rendering.
         /// </summary>
-        private string? _scrollToELementId = null;        
+        private string? _scrollToELementId = null;
 
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// Set to true to enable editing controls for brokers. 
+        /// </summary>
+        [Parameter]
+        public bool EnableEditingControls { get; set; } = false;
 
         /// <summary>
         /// A collection of housing objects to show. 
@@ -45,47 +72,94 @@ namespace FribergFastigheter.Client.Components.Housing
         [Parameter]
         public List<HousingViewModel> Housings { get; set; } = new();
 
-#pragma warning disable CS8618
         /// <summary>
         /// Injected JavaScript runtime.
         /// </summary>
         [Inject]
-        public IJSRuntime JSRuntime {  get; set; }
-#pragma warning restore CS8618
+#pragma warning disable CS8618 
+		public IJSRuntime JSRuntime {  get; set; }
+#pragma warning restore CS8618 
 
-        /// <summary>
-        /// Set to true to truncate long descriptions
-        /// </summary>
-        [Parameter]
+		/// <summary>
+		/// Set to true to truncate long descriptions
+		/// </summary>
+		[Parameter]
         public bool TruncateLongDescriptions { get; set; } = false;
 
-        #endregion
+		#endregion
 
-        #region Method				
+		#region OverriddenMethods
 
-        /// <summary>
-        /// Method invoked after each time the component has been rendered interactively
-        /// and the UI has finished updating (for example, after elements have been added
-        /// to the browser DOM). Any Microsoft.AspNetCore.Components.ElementReference fields
-        /// will be populated by the time this runs. This method is not invoked during prerendering
-        /// or server-side rendering, because those processes are not attached to any live
-        /// browser DOM and are already complete before the DOM is updated. Note that the
-        /// component does not automatically re-render after the completion of any returned
-        /// System.Threading.Tasks.Task, because that would cause an infinite render loop.
-        /// </summary>
-        /// <param name="firstRender">Set to true if this is the first time Microsoft.AspNetCore.Components.ComponentBase.OnAfterRender(System.Boolean)
-        ///     has been invoked on this component instance; otherwise false.</param>
-        /// <returns>A System.Threading.Tasks.Task representing any asynchronous operation.</returns>
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+		/// <inheritdoc/>>
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			await base.OnAfterRenderAsync(firstRender);
+
+			if (_scrollToELementId != null)
+			{
+				await JSRuntime.InvokeVoidAsync("scrollToElement", _scrollToELementId);
+				_scrollToELementId = null;
+			}
+		}
+
+		/// <inheritdoc/>
+		protected override async Task OnParametersSetAsync()
+		{
+			await base.OnParametersSetAsync();
+			RemoveHousingFilter();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Scrolls to the element.
+		/// </summary>
+		/// <param name="housing">The housing object to scroll to.</param>
+		public void ScrollToElement(HousingViewModel housing)
+		{
+			_scrollToELementId = $"HousingListItem-{housing.HousingId}";
+		}
+
+		/// <summary>
+		/// Scrolls to the first element.
+		/// </summary>
+		public void ScrollToFirstElement()
+		{
+			if (Housings.Count > 0)
+			{
+				ScrollToElement(Housings.First());
+			}
+		}
+
+		/// <summary>
+		/// Filters the housing list by broker. 
+		/// </summary>
+		/// <param name="broker"></param>
+		private void FilterByBroker(BrokerViewModel broker)
+		{
+			_currentListFilter = FilterTypes.Brokers;
+			_filteredhousings = Housings.Where(x => x.Broker.BrokerId == broker.BrokerId).ToList();
+		}
+
+		/// <summary>
+		/// Returns a list of brokers from the housing collection.
+		/// </summary>
+		/// <returns>A collection of <see cref="BrokerViewModel"/>.</returns>
+		private List<BrokerViewModel> GetBrokers()
         {
-            await base.OnAfterRenderAsync(firstRender);
-
-            if (_scrollToELementId != null )
-            {
-                await JSRuntime.InvokeVoidAsync("scrollToElement", _scrollToELementId);
-                _scrollToELementId = null;
-            }
-        }
+            return Housings.Select(x => x.Broker)
+                    .GroupBy(
+                    brokerKey => brokerKey.BrokerId,
+                    broker => broker,
+                    (brokerId, brokers) => new
+                    {
+                        Broker = brokers.First()
+                    })
+                    .Select(x => x.Broker)
+                    .ToList();
+        }        
 
         /// <summary>
         /// Event handler for when the create housing button is clicke.d
@@ -103,6 +177,7 @@ namespace FribergFastigheter.Client.Components.Housing
         {
             Housings.Add(createdHousing);
             _isCreatingHousing = false;
+            RemoveHousingFilter();
             ScrollToElement(createdHousing);
         }
 
@@ -123,33 +198,22 @@ namespace FribergFastigheter.Client.Components.Housing
             Housings.Remove(housing);
         }
 
-        /// <summary>
-        /// Event handler for when a list item have transformed into another form. 
-        /// </summary>
-        /// <param name="housing"></param>
-        private void OnListItemTransformed(HousingViewModel housing)
-        {
-            ScrollToElement(housing);
-        }
-
-        /// <summary>
-        /// Scrolls to the element.
-        /// </summary>
-        /// <param name="housing">The housing object to scroll to.</param>
-        public void ScrollToElement(HousingViewModel housing)
-        {
-            _scrollToELementId = $"HousingListItem-{housing.HousingId}";
-        }
-
-        /// <summary>
-		/// Scrolls to the first element.
+		/// <summary>
+		/// Event handler for when a list item have transformed into another form. 
 		/// </summary>
-		public void ScrollToFirstElement()
+		/// <param name="housing"></param>
+		private void OnListItemTransformed(HousingViewModel housing)
+		{
+			ScrollToElement(housing);
+		}
+
+		/// <summary>
+		/// Removes the housing filter.
+		/// </summary>
+		private void RemoveHousingFilter()
         {
-            if (Housings.Count > 0)
-            {
-                ScrollToElement(Housings.First());
-            }
+            _currentListFilter = FilterTypes.All;
+            _filteredhousings = Housings;
         }
 
         #endregion
