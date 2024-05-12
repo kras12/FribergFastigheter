91b27f61af73a5ed2171b0aece5ffb3c22b1a91f
runehero <runehero@hotmail.com>
2024-05-12 13:29:27 +0200

    Created a new component that can dynamically embedd content inside a link element.

---
+++ FribergFastigheter.Client/Components/Links/DynamicLink.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FribergFastigheter.Client.Components.Links
+{
+    /// <summary>
+    /// Component that can dynamically embedd content inside a link element. 
+    /// </summary>
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public partial class DynamicLink : ComponentBase
+    {
+        #region Properties
+
+        /// <summary>
+        /// The body of the component. 
+        /// </summary>
+        [Parameter]
+#pragma warning disable CS8618 
+        public RenderFragment ChildContent { get; set; }
+#pragma warning restore CS8618 
+
+        /// <summary>
+        /// Set to true to enable the link set in <see cref="Url"/>.
+        /// </summary>
+        [Parameter]
+        public bool ShowLink { get; set; }
+
+        /// <summary>
+        /// The url of the link to embedd the content within.
+        /// </summary>
+        [Parameter]
+        public string? Url { get; set; } = null;
+
+        #endregion
+    }
+}
