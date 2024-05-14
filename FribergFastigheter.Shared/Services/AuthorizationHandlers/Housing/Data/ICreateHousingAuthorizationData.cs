bef95fc8f0accaf600ee47f73d3c107bb8fcf8f6
Marcus Mellberg <marcus.mellberg@gmail.com>
2024-05-14 15:13:31 +0200

    Moved all Housing Authorization service files into suitable folders.

---
+++ FribergFastigheter.Shared/Services/AuthorizationHandlers/Housing/Data/ICreateHousingAuthorizationData.cs
@@ -0,0 +1,15 @@
+﻿namespace FribergFastigheter.Shared.Services.AuthorizationHandlers.Housing.Data
+{
+    /// <summary>
+    /// Interface designed to be used with the <see cref="ManageHousingAuthorizationHandler"/> to enable authorization for creating new housing objects.
+    /// </summary>
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public interface ICreateHousingAuthorizationData
+    {
+        /// <summary>
+        /// The new housing broker ID.
+        /// </summary>
+        public int? NewHousingBrokerId { get; }
+    }
+}
