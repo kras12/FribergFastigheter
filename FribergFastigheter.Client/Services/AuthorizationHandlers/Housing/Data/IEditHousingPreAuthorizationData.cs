e6c0b2f0fa4f1a43b1f718788a3734001a288038
runehero <runehero@hotmail.com>
2024-05-14 15:12:32 +0200

    Moved housing authorization service classes to a new folder and splitted the IHousingPreAuthorizationData inteface into 2 new interfaces.

---
+++ FribergFastigheter.Client/Services/AuthorizationHandlers/Housing/Data/IEditHousingPreAuthorizationData.cs
@@ -0,0 +1,22 @@
+﻿using FribergFastigheter.Client.Services.AuthorizationHandlers.Housing;
+
+namespace FribergFastigheter.Client.Services.AuthorizationHandlers.Housing.Data
+{
+    /// <summary>
+    /// Interface designed to be used with the <see cref="ManageHousingPreAuthorizationHandler"/> to enable authorization for editing housing objects.
+    /// </summary>
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public interface IEditHousingPreAuthorizationData
+    {
+        /// <summary>
+        /// The existing housing broker firm ID.
+        /// </summary>
+        public int ExistingHousingBrokerFirmId { get; }
+
+        /// <summary>
+        /// The existing housing broker ID.
+        /// </summary>
+        public int ExistingHousingBrokerId { get; }
+    }
+}
