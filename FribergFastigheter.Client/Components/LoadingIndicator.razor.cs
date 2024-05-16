7f66175e62b7e681b166510a9dcdc37111e10090
runehero <runehero@hotmail.com>
2024-05-16 15:46:46 +0200

    New LoadingIndicator component to be used for show when a page is loading data.

---
+++ FribergFastigheter.Client/Components/LoadingIndicator.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FribergFastigheter.Client.Components
+{
+    /// <summary>
+    /// A component that shows an animated loading element. 
+    /// </summary>
+    /// <!-- Author: Jimmie -->
+    /// <!-- Co Authors: -->
+    public partial class LoadingIndicator : ComponentBase
+    {
+
+    }
+}
