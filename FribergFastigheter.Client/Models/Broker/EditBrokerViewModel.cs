71d2b8a817fc617fd64ae1082312ee27846228c6
runehero <runehero@hotmail.com>
2024-05-16 22:37:01 +0200

    Merged the edit broker view models.

--- FribergFastigheter.Client/Models/Broker/EditBrokerViewModel.cs
+++ FribergFastigheter.Client/Models/Broker/EditBrokerViewModel.cs
@@ -24,6 +24,31 @@ namespace FribergFastigheter.Client.Models.Broker
         public string Description { get; set; } = "";
 
         /// <summary>
+        /// The email of the broker.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+        /// <summary>
+        /// The first name of the broker.
+        /// </summary>
+        [Required]
+        [RegularExpression(NameValidationExpression, ErrorMessage = NameValidationErrorMessage)]
+        public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// The last name of the broker.
+        /// </summary>
+        [Required]
+        [RegularExpression(NameValidationExpression, ErrorMessage = NameValidationErrorMessage)]
+        public string LastName { get; set; } = "";
+
+        /// <summary>
+        /// The phone number of the broker.
+        /// </summary>
+        public string PhoneNumber { get; set; } = "";
+
+        /// <summary>
         /// The broker profile image.
         /// </summary>
         public ImageViewModel? ProfileImage { get; set; }
